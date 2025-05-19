3f7ce07bcceb8971a9811dac5a651d5f70e8526c
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-05-20 00:23:07 +0200

    Initial commit

---
+++ src/test/java/edu/example/fin_tech_app/TestFinTechAppApplication.java
@@ -0,0 +1,11 @@
+package edu.example.fin_tech_app;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFinTechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FinTechAppApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ src/test/java/edu/example/fin_tech_app/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package edu.example.fin_tech_app;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
