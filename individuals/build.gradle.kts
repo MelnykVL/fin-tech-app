381cec25a76ad1c9ed4ba114ffef4b39bdd72db4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-03 19:27:37 +0200

    Removed spring-boot-starter-web from individuals module

--- individuals/build.gradle.kts
+++ individuals/build.gradle.kts
@@ -27,7 +27,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     compileOnly("org.projectlombok:lombok")
