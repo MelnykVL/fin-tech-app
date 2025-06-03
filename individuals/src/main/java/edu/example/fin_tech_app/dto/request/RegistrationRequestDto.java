b538d4bb4d5d128d5c3c8b0242efc80d8123db54
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-03 18:46:16 +0200

    Created RegistrationRequestDto.java

---
+++ individuals/src/main/java/edu/example/fin_tech_app/dto/request/RegistrationRequestDto.java
@@ -0,0 +1,8 @@
+package edu.example.fin_tech_app.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegistrationRequestDto(@Email @NotBlank String email, @Size(min = 8) String password,
+                                     @NotBlank String confirm_password) { }
