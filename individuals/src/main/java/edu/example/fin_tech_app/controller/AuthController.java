161fc6b47d7720331263df8a31276803806f0b76
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-04 16:15:16 +0200

    Implemented reactive /registration endpoint in AuthController.java

--- individuals/src/main/java/edu/example/fin_tech_app/controller/AuthController.java
+++ individuals/src/main/java/edu/example/fin_tech_app/controller/AuthController.java
@@ -1,10 +1,18 @@
 package edu.example.fin_tech_app.controller;
 
+import edu.example.fin_tech_app.dto.request.RegistrationRequest;
+import edu.example.fin_tech_app.dto.response.TokenResponse;
 import edu.example.fin_tech_app.service.TokenService;
 import edu.example.fin_tech_app.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-@RestController
+@RestController("/v1/auth")
 public class AuthController {
 
   private final TokenService tokenService;
@@ -13,5 +21,13 @@ public class AuthController {
   public AuthController(TokenService tokenService, UserService userService) {
     this.tokenService = tokenService;
     this.userService = userService;
+  }
+
+  @PostMapping("/registration")
+  public Mono<ResponseEntity<TokenResponse>> registration(
+      @Valid @RequestBody Mono<RegistrationRequest> registrationRequest) {
+    return registrationRequest.flatMap(userService::register)
+        .map(tr -> ResponseEntity.status(HttpStatus.CREATED)
+            .body(tr));
   }
 }
