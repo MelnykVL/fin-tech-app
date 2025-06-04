d3b87b80869c7a23c4449bfcb29a7e19bc233326
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-05 01:57:05 +0200

    Created ValidationExceptionHandler.java

---
+++ individuals/src/main/java/edu/example/fin_tech_app/exception/ValidationExceptionHandler.java
@@ -0,0 +1,35 @@
+package edu.example.fin_tech_app.exception;
+
+import edu.example.fin_tech_app.dto.response.ErrorResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+  @ExceptionHandler(WebExchangeBindException.class)
+  public Mono<ResponseEntity<ErrorResponse>> handleValidationExceptions(WebExchangeBindException ex) {
+    String errors = ex.getBindingResult()
+        .getAllErrors()
+        .stream()
+        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+        .collect(Collectors.joining(" "));
+
+    ErrorResponse errorResponse = new ErrorResponse(errors, HttpStatus.BAD_REQUEST.value());
+    return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(errorResponse));
+  }
+
+  @ExceptionHandler(Exception.class)
+  public Mono<ResponseEntity<String>> handleGenericException(Exception ex, ServerWebExchange exchange) {
+    return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(ex.getMessage()));
+  }
+}
