18d6bfe760fba2375f68bb0b7b8c03a3e21ee3f4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-05 01:35:34 +0200

    Added messages to fields of RegistrationRequest.java and added password and confirmPassword checker

--- individuals/src/main/java/edu/example/fin_tech_app/dto/request/RegistrationRequest.java
+++ individuals/src/main/java/edu/example/fin_tech_app/dto/request/RegistrationRequest.java
@@ -1,8 +1,20 @@
 package edu.example.fin_tech_app.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record RegistrationRequest(@Email @NotBlank String email, @Size(min = 8) String password,
-                                  @NotBlank String confirm_password) { }
+public record RegistrationRequest(@Email(message = "The email format must be correct.") @NotBlank(
+    message = "The email must not be blank.") String email,
+                                  @Size(min = 8, message = "Password must contain more than 8 characters.") @NotBlank(
+                                      message = "The password must not be blank.") String password,
+                                  @NotBlank(message = "The confirm_password must not be blank.") @JsonProperty(
+                                      "confirm_password") String confirmPassword) {
+
+  @AssertTrue(message = "The password and confirm_password must be the same.")
+  private boolean isPasswordsMatch() {
+    return password != null && password.equals(confirmPassword);
+  }
+}
