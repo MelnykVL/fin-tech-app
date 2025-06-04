67161bf7290fadaef5b32551df0f099e73de8839
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-04 16:22:35 +0200

    Created LoginRequest.java

---
+++ individuals/src/main/java/edu/example/fin_tech_app/dto/request/LoginRequest.java
@@ -0,0 +1,7 @@
+package edu.example.fin_tech_app.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(@Email @NotBlank String email, @Size(min = 8) String password) { }
