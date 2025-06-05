c9388aa79f95cfe1ccf62d6cfecd771ae7f7983f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-05 11:20:31 +0200

    Implemented UserService.java.java

--- individuals/src/main/java/edu/example/fin_tech_app/service/UserService.java
+++ individuals/src/main/java/edu/example/fin_tech_app/service/UserService.java
@@ -1,6 +1,32 @@
 package edu.example.fin_tech_app.service;
 
+import edu.example.fin_tech_app.dto.request.LoginRequest;
+import edu.example.fin_tech_app.dto.request.RegistrationRequest;
+import edu.example.fin_tech_app.dto.response.AuthResponse;
+import edu.example.fin_tech_app.dto.response.UserInfoResponse;
+import io.micrometer.observation.ObservationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
-public class UserService { }
+@RequiredArgsConstructor
+public class UserService {
+
+  private final KeycloakService keycloakService;
+
+  public Mono<AuthResponse> register(RegistrationRequest registrationRequest) {
+    return Mono.fromCallable(() -> keycloakService.createUser(registrationRequest))
+        .subscribeOn(Schedulers.boundedElastic())
+        .flatMap(isCreated -> keycloakService.getTokens());
+  }
+
+  public Mono<AuthResponse> login(LoginRequest loginRequest) {
+    return keycloakService.getTokens();
+  }
+
+  public Mono<UserInfoResponse> getUserInfo(String authorization) {
+    return keycloakService.getUserInfo(authorization);
+  }
+}
