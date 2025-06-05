498b2e0e6f9f7188bf5f37177d9d786fde43a8cf
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-05 11:20:23 +0200

    Implemented TokenService.java

--- individuals/src/main/java/edu/example/fin_tech_app/service/TokenService.java
+++ individuals/src/main/java/edu/example/fin_tech_app/service/TokenService.java
@@ -1,6 +1,22 @@
 package edu.example.fin_tech_app.service;
 
+import edu.example.fin_tech_app.dto.request.RefreshTokenRequest;
+import edu.example.fin_tech_app.dto.response.AuthResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
-public class TokenService { }
+@RequiredArgsConstructor
+public class TokenService {
+
+  private final KeycloakService keycloakService;
+
+  public Mono<AuthResponse> getTokens() {
+    return keycloakService.getTokens();
+  }
+
+  public Mono<AuthResponse> refreshToken(RefreshTokenRequest refreshTokenRequest) {
+    return keycloakService.refreshToken(refreshTokenRequest.refreshToken());
+  }
+}
