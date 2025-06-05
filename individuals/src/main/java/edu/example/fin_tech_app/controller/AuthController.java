cee4b1b317385ef8e10bf1974270de19ff4ec775
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-05 11:18:59 +0200

    Added /refresh-token and /me endpoint to AuthController.java

--- individuals/src/main/java/edu/example/fin_tech_app/controller/AuthController.java
+++ individuals/src/main/java/edu/example/fin_tech_app/controller/AuthController.java
@@ -1,15 +1,20 @@
 package edu.example.fin_tech_app.controller;
 
 import edu.example.fin_tech_app.dto.request.LoginRequest;
+import edu.example.fin_tech_app.dto.request.RefreshTokenRequest;
 import edu.example.fin_tech_app.dto.request.RegistrationRequest;
 import edu.example.fin_tech_app.dto.response.AuthResponse;
+import edu.example.fin_tech_app.dto.response.UserInfoResponse;
 import edu.example.fin_tech_app.service.TokenService;
 import edu.example.fin_tech_app.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +44,22 @@ public class AuthController {
   @PostMapping("/login")
   public Mono<ResponseEntity<AuthResponse>> login(@Valid @RequestBody Mono<LoginRequest> loginRequest) {
     return loginRequest.flatMap(userService::login)
-        .map(ar -> ResponseEntity.status(HttpStatus.ACCEPTED)
+        .map(ar -> ResponseEntity.status(HttpStatus.OK)
+            .body(ar));
+  }
+
+  @PostMapping("/refresh-token")
+  public Mono<ResponseEntity<AuthResponse>> refreshToken(
+      @Valid @RequestBody Mono<RefreshTokenRequest> refreshTokenRequest) {
+    return refreshTokenRequest.flatMap(tokenService::refreshToken)
+        .map(ar -> ResponseEntity.status(HttpStatus.OK)
+            .body(ar));
+  }
+
+  @GetMapping("/me")
+  public Mono<ResponseEntity<UserInfoResponse>> me(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+    return userService.getUserInfo(authorization)
+        .map(ar -> ResponseEntity.status(HttpStatus.OK)
             .body(ar));
   }
 }
