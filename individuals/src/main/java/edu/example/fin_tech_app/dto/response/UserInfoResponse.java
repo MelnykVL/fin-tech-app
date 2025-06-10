57cdaeb6eac86d3bd66c4744a1cbb12e56023de7
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-10 04:02:10 +0200

    Set up UserInfoResponse.java for deserialization

--- individuals/src/main/java/edu/example/fin_tech_app/dto/response/UserInfoResponse.java
+++ individuals/src/main/java/edu/example/fin_tech_app/dto/response/UserInfoResponse.java
@@ -1,6 +1,14 @@
 package edu.example.fin_tech_app.dto.response;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.sql.Timestamp;
 import java.util.List;
 
-public record UserInfoResponse(String id, String email, List<String> roles, LocalDateTime createdAt) { }
+public record UserInfoResponse(@JsonAlias("sub") String id, @JsonProperty("email") String email,
+                               @JsonProperty("roles") List<String> roles,
+                               @JsonProperty("created_at") @JsonFormat(shape = JsonFormat.Shape.STRING,
+                                       timezone = "UTC") Timestamp createdAt) {
+}
