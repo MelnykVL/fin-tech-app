c41f95d69fba4d7fd4d68c6a27656e9021a643b2
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-10 04:03:14 +0200

    Fixed TokenService.java and UserService.java according to changes in KeycloakClient.java

--- individuals/src/main/java/edu/example/fin_tech_app/service/TokenService.java
+++ individuals/src/main/java/edu/example/fin_tech_app/service/TokenService.java
@@ -1,5 +1,6 @@
 package edu.example.fin_tech_app.service;
 
+import edu.example.fin_tech_app.dto.request.LoginRequest;
 import edu.example.fin_tech_app.dto.request.RefreshTokenRequest;
 import edu.example.fin_tech_app.dto.response.AuthResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +11,13 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class TokenService {
 
-  private final KeycloakService keycloakService;
+    private final KeycloakClient keycloakClient;
 
-  public Mono<AuthResponse> getTokens() {
-    return keycloakService.getTokens();
-  }
+    public Mono<AuthResponse> getTokens(LoginRequest loginRequest) {
+        return keycloakClient.getTokens(loginRequest.email(), loginRequest.password());
+    }
 
-  public Mono<AuthResponse> refreshToken(RefreshTokenRequest refreshTokenRequest) {
-    return keycloakService.refreshToken(refreshTokenRequest.refreshToken());
-  }
+    public Mono<AuthResponse> refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        return keycloakClient.refreshToken(refreshTokenRequest.refreshToken());
+    }
 }
--- individuals/src/main/java/edu/example/fin_tech_app/service/UserService.java
+++ individuals/src/main/java/edu/example/fin_tech_app/service/UserService.java
@@ -4,29 +4,26 @@ import edu.example.fin_tech_app.dto.request.LoginRequest;
 import edu.example.fin_tech_app.dto.request.RegistrationRequest;
 import edu.example.fin_tech_app.dto.response.AuthResponse;
 import edu.example.fin_tech_app.dto.response.UserInfoResponse;
-import io.micrometer.observation.ObservationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-  private final KeycloakService keycloakService;
+    private final KeycloakClient keycloakClient;
+    private final TokenService tokenService;
 
-  public Mono<AuthResponse> register(RegistrationRequest registrationRequest) {
-    return Mono.fromCallable(() -> keycloakService.createUser(registrationRequest))
-        .subscribeOn(Schedulers.boundedElastic())
-        .flatMap(isCreated -> keycloakService.getTokens());
-  }
+    public Mono<AuthResponse> register(RegistrationRequest registrationRequest) {
+        return keycloakClient.createUser(registrationRequest);
+    }
 
-  public Mono<AuthResponse> login(LoginRequest loginRequest) {
-    return keycloakService.getTokens();
-  }
+    public Mono<AuthResponse> login(LoginRequest loginRequest) {
+        return tokenService.getTokens(loginRequest);
+    }
 
-  public Mono<UserInfoResponse> getUserInfo(String authorization) {
-    return keycloakService.getUserInfo(authorization);
-  }
+    public Mono<UserInfoResponse> getUserInfo(String accessToken) {
+        return keycloakClient.getUserInfo(accessToken);
+    }
 }
