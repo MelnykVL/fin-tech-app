a1e010546016fb07155fdb9c4aa4b483c6207c30
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-10 13:38:09 +0200

    Created unit and integration tests

--- individuals/src/test/java/edu/example/fin_tech_app/TestcontainersConfiguration.java
+++ individuals/src/test/java/edu/example/fin_tech_app/TestcontainersConfiguration.java
@@ -2,52 +2,48 @@ package edu.example.fin_tech_app;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+@Testcontainers
+public class TestcontainersConfiguration {
 
     private static final String KC_PG_USER_PASS = "keycloak";
     private static final String KC_PG_DB = "keycloak_db";
     private static final String POSTGRES_IMAGE = "postgres:17";
     private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak:26.2";
-    private static final String REALM_IMPORT_FILE = "compose/keycloak/realm-config.json";
+    private static final String REALM_IMPORT_FILE = "realm-config.json";
     private static final String REALM = "fintech-app";
     private static final String CLIENT_ID = "individuals";
     private static final String CLIENT_SECRET = "QF8rGFdWsF6xzdgvjrEnVhVAHGlpFqLl";
 
     private static final Network NETWORK = Network.newNetwork();
 
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgres() {
-        return new PostgreSQLContainer<>(POSTGRES_IMAGE)
-                .withDatabaseName(KC_PG_DB)
-                .withUsername(KC_PG_USER_PASS)
-                .withPassword(KC_PG_USER_PASS)
-                .withNetwork(NETWORK)
-                .withNetworkAliases("postgres");
-    }
+    @Container
+    public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(POSTGRES_IMAGE)
+            .withDatabaseName(KC_PG_DB)
+            .withUsername(KC_PG_USER_PASS)
+            .withPassword(KC_PG_USER_PASS)
+            .withNetwork(NETWORK)
+            .withNetworkAliases("postgres");
 
-    @Bean
-    KeycloakContainer keycloak(PostgreSQLContainer postgres) {
-        return new KeycloakContainer(KEYCLOAK_IMAGE)
-                .withEnv("KEYCLOAK_ADMIN", KC_PG_USER_PASS)
-                .withEnv("KEYCLOAK_ADMIN_PASSWORD", KC_PG_USER_PASS)
-                .withEnv("KC_DB", "postgres")
-                .withEnv("KC_DB_USERNAME", postgres.getUsername())
-                .withEnv("KC_DB_PASSWORD", postgres.getPassword())
-                .withEnv("KC_DB_URL_HOST", "postgres")
-                .withEnv("KC_DB_URL_DATABASE", postgres.getDatabaseName())
-                .withNetwork(NETWORK)
-                .dependsOn(postgres)
-                .withRealmImportFile(REALM_IMPORT_FILE);
-    }
+    @Container
+    public static final KeycloakContainer KEYCLOAK = new KeycloakContainer(KEYCLOAK_IMAGE)
+            .withEnv("KEYCLOAK_ADMIN", KC_PG_USER_PASS)
+            .withEnv("KEYCLOAK_ADMIN_PASSWORD", KC_PG_USER_PASS)
+            .withEnv("KC_DB", "postgres")
+            .withEnv("KC_DB_USERNAME", POSTGRES.getUsername())
+            .withEnv("KC_DB_PASSWORD", POSTGRES.getPassword())
+            .withEnv("KC_DB_URL_HOST", "postgres")
+            .withEnv("KC_DB_URL_DATABASE", POSTGRES.getDatabaseName())
+            .withNetwork(NETWORK)
+            .dependsOn(POSTGRES)
+            .withRealmImportFile(REALM_IMPORT_FILE);
 
     @DynamicPropertySource
     static void registry(DynamicPropertyRegistry registry, KeycloakContainer keycloak) {
