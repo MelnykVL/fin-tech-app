1090f1448a8d21c86dd85e2126ca98e359ab23c4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-10 03:56:29 +0200

    Reformated code in AuthResponse.java

--- individuals/src/main/java/edu/example/fin_tech_app/dto/response/AuthResponse.java
+++ individuals/src/main/java/edu/example/fin_tech_app/dto/response/AuthResponse.java
@@ -4,4 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record AuthResponse(@JsonProperty("access_token") String accessToken, @JsonProperty("expires_in") Long expiresIn,
                            @JsonProperty("refresh_token") String refreshToken,
-                           @JsonProperty("token_type") String tokenType) { }
+                           @JsonProperty("token_type") String tokenType) {
+}
