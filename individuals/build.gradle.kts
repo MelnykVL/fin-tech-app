b743b965f1383010a42a16fb41b6e7928caff578
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-19 20:31:29 +0200

    Added spring-boot-starter-oauth2-client dependency to individuals module

--- individuals/build.gradle.kts
+++ individuals/build.gradle.kts
@@ -32,6 +32,7 @@ repositories {
 dependencies {
   implementation(platform(SpringBootPlugin.BOM_COORDINATES))
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
