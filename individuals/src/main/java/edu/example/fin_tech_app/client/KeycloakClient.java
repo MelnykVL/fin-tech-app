ed345b0f95d30cbc7bc267bdaebbede39922bbce
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-20 01:04:27 +0200

    Removed getAdminAccessToken method from KeycloakClient.java

--- individuals/src/main/java/edu/example/fin_tech_app/client/KeycloakClient.java
+++ individuals/src/main/java/edu/example/fin_tech_app/client/KeycloakClient.java
@@ -1,13 +1,13 @@
 package edu.example.fin_tech_app.client;
 
 import edu.example.fin_tech_app.config.KeycloakProps;
+import edu.example.fin_tech_app.dto.request.KeycloakCredentialsRepresentationRequest;
+import edu.example.fin_tech_app.dto.request.KeycloakUserRepresentationRequest;
 import edu.example.fin_tech_app.dto.request.RegistrationRequest;
 import edu.example.fin_tech_app.dto.response.AuthResponse;
 import edu.example.fin_tech_app.dto.response.UserInfoResponse;
 import edu.example.fin_tech_app.exception.KeycloakIntegrationException;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -27,18 +27,20 @@ public class KeycloakClient {
   private static final String TOKEN_ENDPOINT = "/realms/{realm}/protocol/openid-connect/token";
   private static final String USER_ENDPOINT = "/admin/realms/{realm}/users";
   private static final String USERINFO_ENDPOINT = "/realms/{realm}/protocol/openid-connect/userinfo";
+  private static final String DEFAULT_SCOPES = "openid";
+  private static final String KEYCLOAK_CREDENTIALS_REPRESENTATION_TYPE = "password";
 
-  private final WebClient webClient;
+  private final WebClient keycloakWebClient;
+  private final WebClient adminKeycloakWebClient;
   private final KeycloakProps keycloakProps;
 
   public Mono<AuthResponse> createUser(RegistrationRequest registrationRequest) {
-    return getAdminAccessToken()
-        .flatMap(authResponse -> this.createUserInKeycloak(registrationRequest, authResponse.accessToken()))
+    return this.createUserInKeycloak(registrationRequest)
         .flatMap(userEmail -> this.getTokens(registrationRequest.email(), registrationRequest.password()));
   }
 
   public Mono<AuthResponse> getTokens(String email, String password) {
-    return this.getTokens(email, password, "openid");
+    return this.getTokens(email, password, DEFAULT_SCOPES);
   }
 
   public Mono<AuthResponse> getTokens(String username, String password, String scope) {
@@ -50,7 +52,7 @@ public class KeycloakClient {
     formData.add(OAuth2ParameterNames.GRANT_TYPE, AuthorizationGrantType.PASSWORD.getValue());
     formData.add(OAuth2ParameterNames.SCOPE, scope);
 
-    return webClient.post()
+    return keycloakWebClient.post()
         .uri(uriBuilder -> uriBuilder.path(TOKEN_ENDPOINT).build(keycloakProps.getRealm()))
         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
         .body(BodyInserters.fromFormData(formData))
@@ -71,7 +73,7 @@ public class KeycloakClient {
     formData.add(OAuth2ParameterNames.GRANT_TYPE, AuthorizationGrantType.REFRESH_TOKEN.getValue());
     formData.add(OAuth2ParameterNames.REFRESH_TOKEN, refreshToken);
 
-    return webClient.post()
+    return keycloakWebClient.post()
         .uri(uriBuilder -> uriBuilder.path(TOKEN_ENDPOINT).build(keycloakProps.getRealm()))
         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
         .body(BodyInserters.fromFormData(formData))
@@ -86,7 +88,7 @@ public class KeycloakClient {
   }
 
   public Mono<UserInfoResponse> getUserInfo(String accessToken) {
-    return webClient.get()
+    return keycloakWebClient.get()
         .uri(uriBuilder -> uriBuilder.path(USERINFO_ENDPOINT).build(keycloakProps.getRealm()))
         .header(HttpHeaders.AUTHORIZATION, accessToken)
         .retrieve()
@@ -99,38 +101,16 @@ public class KeycloakClient {
         .bodyToMono(UserInfoResponse.class);
   }
 
-  private Mono<AuthResponse> getAdminAccessToken() {
-    MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-    formData.add(OAuth2ParameterNames.CLIENT_ID, keycloakProps.getClientId());
-    formData.add(OAuth2ParameterNames.CLIENT_SECRET, keycloakProps.getClientSecret());
-    formData.add(OAuth2ParameterNames.GRANT_TYPE, AuthorizationGrantType.CLIENT_CREDENTIALS.getValue());
+  private Mono<String> createUserInKeycloak(RegistrationRequest registrationRequest) {
+    KeycloakCredentialsRepresentationRequest credentials =
+        new KeycloakCredentialsRepresentationRequest(KEYCLOAK_CREDENTIALS_REPRESENTATION_TYPE,
+            registrationRequest.password(), Boolean.FALSE);
+    KeycloakUserRepresentationRequest user =
+        new KeycloakUserRepresentationRequest(registrationRequest.email(), Boolean.TRUE, List.of("users"),
+            List.of(credentials));
 
-    return webClient.post().uri(uriBuilder -> uriBuilder.path(TOKEN_ENDPOINT).build(keycloakProps.getRealm()))
-        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-        .body(BodyInserters.fromFormData(formData)).retrieve()
-        .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(), resp ->
-            resp.bodyToMono(String.class)
-                .flatMap(errorMessage ->
-                    Mono.just(
-                        new KeycloakIntegrationException("Unnabel to get admin token" + errorMessage, resp.statusCode()
-                            .value()))))
-        .bodyToMono(AuthResponse.class);
-  }
-
-  private Mono<String> createUserInKeycloak(RegistrationRequest registrationRequest, String adminAccessToken) {
-    Map<String, Object> credentials = new LinkedHashMap<>();
-    credentials.put("type", "password");
-    credentials.put("value", registrationRequest.password());
-    credentials.put("temporary", Boolean.FALSE);
-    Map<String, Object> user = new LinkedHashMap<>();
-    user.put("email", registrationRequest.email());
-    user.put("enabled", Boolean.TRUE);
-    user.put("groups", List.of("users"));
-    user.put("credentials", List.of(credentials));
-
-    return webClient.post()
+    return adminKeycloakWebClient.post()
         .uri(uriBuilder -> uriBuilder.path(USER_ENDPOINT).build(keycloakProps.getRealm()))
-        .header(HttpHeaders.AUTHORIZATION, "Bearer " + adminAccessToken)
         .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(user).retrieve()
         .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(), resp ->
